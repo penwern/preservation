b5f4426269723a33cd08ca194280d11395a28fd8
camlyall <cameron@penwern.co.uk>
2024-08-28 10:12:40 +0100

    Fix non admin user preservation

--- preservation/curate.py
+++ preservation/curate.py
@@ -15,19 +15,27 @@ class CurateManager:
         self._url: str = curate_url
         self._token: str = None
         self._token_timeout: datetime = datetime.min
+        self._admin_token: str = None
+        self._admin_token_timeout: datetime = datetime.min
         self._configure_cells_client()
 
-    @property
-    def token(self) -> str:
-        if self._has_expired():
-            self._gen_new_token()
-        return self._token
+    def token(self, user) -> str:
+        if user == 'admin':
+            if self._has_expired(self._admin_token_timeout):
+                self._admin_token = self._gen_new_token('admin')
+                self._admin_token_timeout = datetime.now() + timedelta(minutes=token_timeout_minutes)
+            return self._admin_token
+        else:
+            if self._has_expired(self._token_timeout):
+                self._token = self._gen_new_token(user)
+                self._token_timeout = datetime.now() + timedelta(minutes=token_timeout_minutes)
+            return self._token
 
-    def _has_expired(self) -> bool:
-        return datetime.now() >= self._token_timeout
+    def _has_expired(self, timeout) -> bool:
+        return datetime.now() >= timeout
 
-    def _gen_new_token(self):
-        commands = ['cells', 'admin', 'user', 'token', '-u', self._user, '-e', f'{token_timeout_minutes}m', '--quiet']
+    def _gen_new_token(self, user):
+        commands = ['cells', 'admin', 'user', 'token', '-u', user, '-e', f'{token_timeout_minutes}m', '--quiet']
         try:
             result = subprocess.run(commands, capture_output=True, text=True, check=True, timeout=5)
             result.check_returncode()
@@ -42,12 +50,10 @@ class CurateManager:
         if not token:
             raise RuntimeError("Generated token is empty.")
         
-        self._token = token
-        self._token_timeout = datetime.now() + timedelta(minutes=token_timeout_minutes)
-        self._configure_cells_client()
+        return token
 
     def _configure_cells_client(self):
-        commands = ['cec', 'configure', 'token', '--url', self._url, '--login', self._user, '--token', self.token]
+        commands = ['cec', 'configure', 'token', '--url', self._url, '--login', self._user, '--token', self.token(self._user)]
         try:
             output = subprocess.run(commands, capture_output=True, text=True, check=True)
             output.check_returncode()
@@ -56,40 +62,51 @@ class CurateManager:
             raise RuntimeError("Failed to configure Cells Client.") from e
 
     def update_tag(self, node_id: str, tag: str):
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.token}'
-        }
-        payload = json.dumps({
-            "MetaDatas": [
-                {
-                    "JsonValue": f"\"{tag}\"",
-                    "Namespace": "usermeta-a3m-progress",
-                    "NodeUuid": node_id
-                }
-            ],
-            "Operation": "PUT"
-        })
-        endpoint = f'{self._url}/a/user-meta/update'
-        response = requests.put(endpoint, headers=headers, data=payload)
-        response.raise_for_status()
-        logger.debug(f"Tag: {tag} updated for node: {node_id}")
+        try:
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f"Bearer {self.token(self._user)}"
+            }
+            payload = json.dumps({
+                "MetaDatas": [
+                    {
+                        "JsonValue": f"\"{tag}\"",
+                        "Namespace": "usermeta-a3m-progress",
+                        "NodeUuid": node_id
+                    }
+                ],
+                "Operation": "PUT"
+            })
+            endpoint = f'{self._url}/a/user-meta/update'
+            response = requests.put(endpoint, headers=headers, data=payload)
+            response.raise_for_status()
+            logger.debug(f"Tag: {tag} updated for node: {node_id}")
+        except Exception as e:
+            err_msg = f"Unexpected error: {e}"
+            logger.error(err_msg)
+            raise RuntimeError(err_msg) from e
 
     def gather_child_nodes(self, parent_curate_node_path: str) -> list:
         logger.info(f"Gathering children of {parent_curate_node_path}")
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.token}'
-        }
-        payload = json.dumps({
-            "Node": {
-                "Path": str(parent_curate_node_path)
-            },
-            "Recursive": True
-        })
-        endpoint = f"{self._url}/a/tree/admin/list"
-        response = requests.post(endpoint, headers=headers, data=payload)
-        response.raise_for_status()
+        try:
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f"Bearer {self.token('admin')}"
+            }
+            payload = json.dumps({
+                "Node": {
+                    "Path": str(parent_curate_node_path)
+                },
+                "Recursive": True
+            })
+            endpoint = f"{self._url}/a/tree/admin/list"
+            response = requests.post(endpoint, headers=headers, data=payload)
+            response.raise_for_status()
+        
+        except Exception as e:
+            err_msg = f"Unexpected error: {e}"
+            logger.error(err_msg)
+            raise RuntimeError(err_msg) from e
         return response.json().get('Children', [])
 
     def download_node(self, destination_path: Path, node_path: Path) -> Path:
